ab9bc68f6d4177b82b3907cb901e7abe51261104
Juan Torres Mancheño <j.torresmancheno@edu.gva.es>
2023-05-08 17:47:07 +0200

    mcgapi.js

---
+++ mcgapi.js
@@ -0,0 +1,22 @@
+function prova(){
+  alert("¿Cómo estás?")
+}
+
+function postBadge(e){
+  fetch('https://juantoman-json-server.glitch.me/badges', {
+    method: 'POST',
+    body: JSON.stringify({
+      name: "Badge1",
+      description: "Badge1",
+      "XP": 100
+    }),
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8',
+    },
+  })
+  .then((response) => response.json())
+  .then((json) => {
+    console.log(json)
+    alert(e.target.closest(".genially-animated-wrapper").id)
+  });
+}
