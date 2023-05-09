8d89009802aee085741b46e4893ddf503577a383
Juan Torres Mancheño <j.torresmancheno@edu.gva.es>
2023-05-09 23:24:19 +0200

    prova

--- mcglogin.js
+++ mcglogin.js
@@ -1,12 +1,4 @@
-(function (d) { 
-var js, id = "genially-embed-js", ref = d.getElementsByTagName("script")[0];
-if (d.getElementById(id)) { return; }
-js = d.createElement("script"); 
-js.id = id; 
-js.async = true; 
-js.src = "https://www.myclassgame.es/mcgapi/genially/sweetalert2.min.js"; 
-ref.parentNode.insertBefore(js, ref); }
-(document));
+
 function showStudentData(studentCreds) {
     const student=studentCreds;    
     let id=student.studentCode;
