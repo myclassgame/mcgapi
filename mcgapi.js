942a2276f69acc4d810894f1a70b3d58b38fcba3
Juan Torres Mancheño <j.torresmancheno@edu.gva.es>
2023-06-05 13:49:04 +0200

    load newclassBtn

--- mcgapi.js
+++ mcgapi.js
@@ -236,6 +236,8 @@ function loadClassesMCG() {
     })
     .then(function(data) {
       console.log(data); // Manejar la respuesta recibida del servidor
+      const newclassBtn = document.querySelector("[data-title='newclassMCG']");
+      console.log(newclassBtn)
     })
     .catch(function(error) {
       console.log('Error:', error);
