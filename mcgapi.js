2d4564fd08e25efd08efca0d0d26ebd681aafa9d
Juan Torres Mancheño <j.torresmancheno@edu.gva.es>
2023-06-14 23:27:14 +0200

    Update mcgapi.js, postitMCG.style.opacity=0/1

--- mcgapi.js
+++ mcgapi.js
@@ -268,6 +268,7 @@ async function loadClassesMCG() {
       /*postitMCG.style.overflowY="auto";
       postitMCG.style.overflowX="hidden";
       postitMCG.style.pointerEvents="auto";*/
+      postitMCG.style.opacity=0;
       if (!document.querySelector("#myclassesMCG")) {
         const myclassesMCG = document.createElement('div');
         myclassesMCG.id = 'myclassesMCG';
@@ -289,7 +290,7 @@ async function loadClassesMCG() {
       })
       
       //Visualizar postitMCG
-      postitMCG.classList.add('myclassesVisible');
+      postitMCG.style.opacity=1;
       //Ocultar waitingMCG
       document.querySelector("[data-title='waitingMCG']").classList.add("hiddenElement");
     
