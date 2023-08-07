c28384be68d7d5d41c56dea02089e460593b3dae
Juan Torres Mancheño <j.torresmancheno@edu.gva.es>
2023-08-07 09:42:44 +0200

    Update studentPage.js cargar nombre

--- studentPage.js
+++ studentPage.js
@@ -1,4 +1,27 @@
 function loadStudentPage() {
   console.log('loadStudentPage');
-  console.log(generarClaveAleatoria())
+  console.log(generarClaveAleatoria());
+  const studentIdMCG = window.localStorage.getItem("studentIdMCG");
+  fetch('https://genialmcg.glitch.me/students/?studentId='+studentIdMCG)
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(data) {
+      console.log(data); // Manejar la respuesta recibida del servidor
+      
+      const studentBtn = document.querySelector("[data-title='studentMCG']");
+      //studentBtn.addEventListener("click", loadStudentMCG)
+      studentBtn.querySelector('span span').textContent = data.studentName;
+      studentBtn.querySelector('.color1').style.fill = data.color;
+      studentBtn.id=data.studentId;
+      
+      //Ocultar waitingMCG
+      document.querySelector("[data-title='waitingMCG']").classList.add("hiddenElement");
+    
+    })
+    .catch(function(error) {
+      console.log('Error:', error);
+      //Ocultar waitingMCG
+      document.querySelector("[data-title='waitingMCG']").classList.add("hiddenElement");
+    });
 }
