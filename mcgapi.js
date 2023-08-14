71bac962372e23b8f498e9d8adac92684e7ad3a7
Juan Torres Mancheño <j.torresmancheno@edu.gva.es>
2023-08-14 09:08:55 +0200

    Update mcgapi.js error nombre fichero

--- mcgapi.js
+++ mcgapi.js
@@ -2,7 +2,7 @@
 (function (d) { 
 var js, id = "genially-embed-js", ref = d.getElementsByTagName("script")[0];
 if (d.getElementById(id)) { return; }
-var files = [ "sweetalert2.min.js", "studentPage.js", "studentsPage.js", "functionsMCG.js", "classesPages.js" ]
+var files = [ "sweetalert2.min.js", "studentPage.js", "studentsPage.js", "functionsMCG.js", "classesPage.js" ]
 files.forEach(file =>   {
   js = d.createElement("script"); 
   js.id = id; 
