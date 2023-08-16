ba8430b19221de874a17e042012af83547f99aef
Juan Torres Mancheño <j.torresmancheno@edu.gva.es>
2023-08-16 17:16:41 +0200

    Update studentPage.js PATCH student.id

--- studentPage.js
+++ studentPage.js
@@ -1,36 +1,22 @@
 function loadStudentPage() {
   console.log('loadStudentPage');
   console.log(generarClaveAleatoria());
-  const studentIdMCG = window.localStorage.getItem("studentIdMCG");
-  fetch('https://genialmcg.glitch.me/students/?studentId='+studentIdMCG)
-    .then(function(response) {
-      return response.json();
-    })
-    .then(function(data) {
-      console.log(data); // Manejar la respuesta recibida del servidor
-      let student = data[0]
-      const studentBtn = document.querySelector("[data-title='studentMCG']");
-      //studentBtn.addEventListener("click", loadStudentMCG)
-      studentBtn.querySelector('span span').textContent = student.studentName;
-      studentBtn.querySelector('.color1').style.fill = student.color;
-      studentBtn.id=student.studentId;
-      const studentPoints=document.querySelectorAll("[data-title='studentPoints'] .sc-FNXRL .genially-view-text span")
-      studentPoints[0].textContent = student.XP;
-      studentPoints[1].textContent = student.HP;
-      studentPoints[2].textContent = student.GP;
-      studentPoints[3].textContent = student.AP;
+  const student = JSON.parse(window.localStorage.getItem("studentMCG"));
+  const studentBtn = document.querySelector("[data-title='studentMCG']");
+  //studentBtn.addEventListener("click", loadStudentMCG)
+  studentBtn.querySelector('span span').textContent = student.studentName;
+  studentBtn.querySelector('.color1').style.fill = student.color;
+  studentBtn.id=student.studentId;
+  const studentPoints=document.querySelectorAll("[data-title='studentPoints'] .sc-FNXRL .genially-view-text span")
+  studentPoints[0].textContent = student.XP;
+  studentPoints[1].textContent = student.HP;
+  studentPoints[2].textContent = student.GP;
+  studentPoints[3].textContent = student.AP;
 
-      document.querySelector("[data-title='studentPoints']").addEventListener("click", studentPointsBtn)
-                                
-      //Ocultar waitingMCG
-      document.querySelector("[data-title='waitingMCG']").classList.add("hiddenElement");
-    
-    })
-    .catch(function(error) {
-      console.log('Error:', error);
-      //Ocultar waitingMCG
-      document.querySelector("[data-title='waitingMCG']").classList.add("hiddenElement");
-    });
+  document.querySelector("[data-title='studentPoints']").addEventListener("click", studentPointsBtn)
+                            
+  //Ocultar waitingMCG
+  document.querySelector("[data-title='waitingMCG']").classList.add("hiddenElement");
 }
 
 async function studentPointsBtn() {
@@ -80,10 +66,10 @@ async function studentPointsBtn() {
     };
 
     
-    const studentIdMCG = window.localStorage.getItem("studentIdMCG");
+    const student = JSON.parse(window.localStorage.getItem("studentMCG"));
 
     // Realizar la solicitud fetch PATCH
-    fetch('https://genialmcg.glitch.me/students/1', options)
+    fetch('https://genialmcg.glitch.me/students/' + student.id, options)
       .then(function(response) {
         return response.json();
       })
