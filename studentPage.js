98b4a6f3e2d536af0126c0e898cbffc746d9c24a
Juan Torres Mancheño <j.torresmancheno@edu.gva.es>
2023-08-21 14:40:32 +0200

    Update studentPage.js delete

--- studentPage.js
+++ studentPage.js
@@ -123,9 +123,9 @@ function deleteStudent() {
   const studentId = studentMCG.id;
   console.log("Borrar " + studentId)
 
-  /*fetch('https://genialmcg.glitch.me/students/' + studentMCG.id, {
+  fetch('https://genialmcg.glitch.me/students/' + studentMCG.id, {
     method: 'DELETE',
-  });*/
+  });
 
   document.querySelector("[data-title='returnBtn']").click();
   
