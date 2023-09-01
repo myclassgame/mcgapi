78639464c6002cd3707e97150d9eb0412b299158
Juan Torres Mancheño <j.torresmancheno@edu.gva.es>
2023-09-01 08:11:05 +0000

    Update mcg4gen.js loginMCG con SWAL

--- mcg4gen.js
+++ mcg4gen.js
@@ -12,6 +12,23 @@ files.forEach(file =>   {
 })}
 (document));
 
+//Función mensaje SWAL
+function mensajeSWAL(mensaje){
+  Swal.fire({
+      title: '<span style="color:yellow;">@</span><span style="color:red;">My</span><span style="color:blue;">Class</span><span style="color:lime;">Game</span>',
+      background: '#268bd2',
+      showCloseButton: true,
+      confirmButtonColor: '#d33',
+      confirmButtonText: 'OK',
+      imageUrl: 'https://www.myclassgame.es/images/@mcgnb.png',
+      imageWidth: 75,
+      imageHeight: 75,
+      imageAlt: '@MyClassGame',
+      html:
+          '<h2 style="color: white">' + mensaje + '</h2>',
+  })
+}
+
 function loadEvents(page) {
   console.log("loadEvents")
 
@@ -34,12 +51,89 @@ function loadElementsPage() {
   const mcg4gen = document.querySelector("[data-title='mcg4gen']").addEventListener("click", loginMCG);
 }
 
-function loginMCG() {
+/*function loginMCG() {
   alert("Login")
   //Visualizar nextMCG
   document.querySelector("[data-title='nextMCG']").classList.remove("hiddenElement");
   //Ocultar waitingMCG
   document.querySelector("[data-title='stopMCG']").classList.add("hiddenElement");
+}*/
+
+async function loginMCG(){
+  const { value: formValues } = await Swal.fire({
+      title: '<span style="color:yellow;">@</span><span style="color:red;">My</span><span style="color:blue;">Class</span><span style="color:lime;">Game</span>',
+      background: '#268bd2',
+      showCloseButton: true,
+      showCancelButton: true,
+      confirmButtonColor: '#0f0',
+      confirmButtonText: 'Login',
+      cancelButtonColor: '#d33',
+      imageUrl: 'https://www.myclassgame.es/images/@mcgnb.png',
+      imageWidth: 75,
+      imageHeight: 75,
+      imageAlt: '@MyClassGame',
+      html:
+          '<input id="swal-input1" class="swal2-input" placeholder="StudentName">' +
+          '<input type="password" id="swal-input2" class="swal2-input" placeholder="Code">',
+      focusConfirm: false,
+      preConfirm: () => {
+        //Datos URL
+          let url = window.location.href;
+          let parts = url.split('/');
+          let genId = parts[parts.length - 1];
+          const studentLogin = {
+              studentName: document.getElementById('swal-input1').value,
+              studentId: document.getElementById('swal-input2').value,
+              genId: genId
+          }
+          return studentLogin
+      }
+  })
+  if (formValues) {
+    
+    console.log(formValues)
+    
+    // Objeto de datos que se enviará en la solicitud POST
+    var studentLogin = formValues
+
+    // Configurar opciones para la solicitud fetch POST
+    var options = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(studentLogin)
+    };
+
+    // Realizar la solicitud fetch POST
+    fetch('https://genialmcg.glitch.me/studentLogin', options)
+      .then(function(response) {
+        return response.json();
+      })
+      .then(function(data) {
+        //console.log(data); // Manejar la respuesta recibida del servidor
+           
+        if(!data.status){
+          console.log("Credenciales válidas")
+          
+          const studentLoggedInMCG = {
+            'userId':data.user.userId,
+            'studentName':data.user.studentName,
+            'studentId': data.user.studentId
+          }
+          
+          localStorage.setItem('studentLoggedInMCG',JSON.stringify(studentLoggedInMCG));
+          
+        } else {
+          mensajeSWAL(data.message)
+        }
+      })
+      .catch(function(error) {
+        console.log('Error:', error);
+      
+        alert("Error de conexión")
+      });
+  }
 }
 
 loadElementsPage()
