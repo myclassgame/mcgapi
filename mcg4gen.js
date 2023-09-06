588a5206b2d1397c376f0e644a3e82e918001db7
Juan Torres Mancheño <j.torresmancheno@edu.gva.es>
2023-09-06 07:51:21 +0200

    Update mcg4gen.js logout

--- mcg4gen.js
+++ mcg4gen.js
@@ -60,84 +60,93 @@ function loadElementsPage() {
 }*/
 
 async function loginMCG(){
-  const { value: formValues } = await Swal.fire({
-      title: '<span style="color:yellow;">@</span><span style="color:red;">My</span><span style="color:blue;">Class</span><span style="color:lime;">Game</span>',
-      background: '#268bd2',
-      showCloseButton: true,
-      showCancelButton: true,
-      confirmButtonColor: '#0f0',
-      confirmButtonText: 'Login',
-      cancelButtonColor: '#d33',
-      imageUrl: 'https://www.myclassgame.es/images/@mcgnb.png',
-      imageWidth: 75,
-      imageHeight: 75,
-      imageAlt: '@MyClassGame',
-      html:
-          '<input id="swal-input1" class="swal2-input" placeholder="StudentName">' +
-          '<input type="password" id="swal-input2" class="swal2-input" placeholder="Code">',
-      focusConfirm: false,
-      preConfirm: () => {
-        //Datos URL
-          let url = window.location.href;
-          let parts = url.split('/');
-          let genId = parts[parts.length - 1];
-          const studentLogin = {
-              studentName: document.getElementById('swal-input1').value,
-              studentId: document.getElementById('swal-input2').value,
-              genId: genId
-          }
-          return studentLogin
-      }
-  })
-  if (formValues) {
-    
-    console.log(formValues)
-    
-    // Objeto de datos que se enviará en la solicitud POST
-    var studentLogin = formValues
-
-    // Configurar opciones para la solicitud fetch POST
-    var options = {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(studentLogin)
-    };
-
-    // Realizar la solicitud fetch POST
-    fetch('https://genialmcg.glitch.me/studentLogin', options)
-      .then(function(response) {
-        return response.json();
-      })
-      .then(function(data) {
-        //console.log(data); // Manejar la respuesta recibida del servidor
-           
-        if(!data.status){
-          console.log("Credenciales válidas")
-          
-          const studentLoggedInMCG = {
-            'userId':data.user.userId,
-            'studentName':data.user.studentName,
-            'studentId': data.user.studentId
-          }
-          
-          localStorage.setItem('studentLoggedInMCG',JSON.stringify(studentLoggedInMCG));
-
-          //Visualizar nextMCG
-          document.querySelector("[data-title='nextMCG']").classList.remove("hiddenElement");
-          //Ocultar waitingMCG
-          document.querySelector("[data-title='stopMCG']").classList.add("hiddenElement");
-          
-        } else {
-          mensajeSWAL(data.message)
+  if (!localStorage.getItem('studentLoggedInMCG')) {
+    const { value: formValues } = await Swal.fire({
+        title: '<span style="color:yellow;">@</span><span style="color:red;">My</span><span style="color:blue;">Class</span><span style="color:lime;">Game</span>',
+        background: '#268bd2',
+        showCloseButton: true,
+        showCancelButton: true,
+        confirmButtonColor: '#0f0',
+        confirmButtonText: 'Login',
+        cancelButtonColor: '#d33',
+        imageUrl: 'https://www.myclassgame.es/images/@mcgnb.png',
+        imageWidth: 75,
+        imageHeight: 75,
+        imageAlt: '@MyClassGame',
+        html:
+            '<input id="swal-input1" class="swal2-input" placeholder="StudentName">' +
+            '<input type="password" id="swal-input2" class="swal2-input" placeholder="Code">',
+        focusConfirm: false,
+        preConfirm: () => {
+          //Datos URL
+            let url = window.location.href;
+            let parts = url.split('/');
+            let genId = parts[parts.length - 1];
+            const studentLogin = {
+                studentName: document.getElementById('swal-input1').value,
+                studentId: document.getElementById('swal-input2').value,
+                genId: genId
+            }
+            return studentLogin
         }
-      })
-      .catch(function(error) {
-        console.log('Error:', error);
+    })
+    if (formValues) {
       
-        alert("Error de conexión")
-      });
+      console.log(formValues)
+      
+      // Objeto de datos que se enviará en la solicitud POST
+      var studentLogin = formValues
+  
+      // Configurar opciones para la solicitud fetch POST
+      var options = {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(studentLogin)
+      };
+  
+      // Realizar la solicitud fetch POST
+      fetch('https://genialmcg.glitch.me/studentLogin', options)
+        .then(function(response) {
+          return response.json();
+        })
+        .then(function(data) {
+          //console.log(data); // Manejar la respuesta recibida del servidor
+             
+          if(!data.status){
+            console.log("Credenciales válidas")
+            
+            const studentLoggedInMCG = {
+              'userId':data.user.userId,
+              'studentName':data.user.studentName,
+              'studentId': data.user.studentId
+            }
+            
+            localStorage.setItem('studentLoggedInMCG',JSON.stringify(studentLoggedInMCG));
+  
+            //Visualizar nextMCG
+            document.querySelector("[data-title='nextMCG']").classList.remove("hiddenElement");
+            //Ocultar waitingMCG
+            document.querySelector("[data-title='stopMCG']").classList.add("hiddenElement");
+            
+          } else {
+            mensajeSWAL(data.message)
+          }
+        })
+        .catch(function(error) {
+          console.log('Error:', error);
+        
+          alert("Error de conexión")
+        });
+    }
+  } else {
+    localStorage.removeItem('studentLoggedInMCG')
+    mensajeSWAL("Has cerrado la sesión")
+    //Ocultar nextMCG
+    document.querySelector("[data-title='nextMCG']").classList.add("hiddenElement");
+    //Visualizar waitingMCG
+    document.querySelector("[data-title='stopMCG']").classList.remove("hiddenElement");
   }
 }
 
