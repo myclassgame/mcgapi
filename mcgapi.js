9c50612c1bb147a69d4dafe0867b305ad17d7af2
Juan Torres Mancheño <j.torresmancheno@edu.gva.es>
2023-09-10 10:42:07 +0200

    Update mcgapi.js badgesPage.js

--- mcgapi.js
+++ mcgapi.js
@@ -2,7 +2,7 @@
 (function (d) { 
 var js, id = "genially-embed-js", ref = d.getElementsByTagName("script")[0];
 if (d.getElementById(id)) { return; }
-var files = [ "sweetalert2.min.js", "studentPage.js", "studentsPage.js", "functionsMCG.js", "classesPage.js", "teacherPage.js", "geniallysPage.js" ]
+var files = [ "sweetalert2.min.js", "studentPage.js", "studentsPage.js", "functionsMCG.js", "classesPage.js", "teacherPage.js", "geniallysPage.js", "badgesPage.js" ]
 files.forEach(file =>   {
   js = d.createElement("script"); 
   js.id = id; 
